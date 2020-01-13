17867b9d8b6ccfe66ad2c3e08405b1ae8514742b
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-01-13 20:05:30 +0700

    get current user on created by and updated by. Update masih error

--- my_python_blog/main/models.py
+++ my_python_blog/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from crum import get_current_user
 
 # Create your models here.
 class Status(models.Model):
@@ -8,3 +9,12 @@ class Status(models.Model):
 	updated_at	=	models.DateTimeField(auto_now = True)
 	created_by	=	models.CharField(max_length = 32)
 	updated_by	=	models.CharField(max_length = 32)
+
+	def save(self, *args, **kwargs):
+		user	=	get_current_user()
+		if user and not user.pk:
+			user	=	None
+		if not self.pk:
+			self.created_by	=	user.pk
+			self.updated_by	=	user.pk
+			super(Status, self).save(*args, **kwargs)
