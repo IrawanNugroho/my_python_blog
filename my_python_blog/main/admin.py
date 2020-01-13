f4f8e7e3fa219b27efaa188983407f02bb3d7ad1
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-01-13 14:46:39 +0700

    status admin page

--- my_python_blog/main/admin.py
+++ my_python_blog/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from . import models
+
 
 # Register your models here.
+class StatusAdmin(admin.ModelAdmin):
+    list_display    =   ('name', 'description', 'created_by', 'updated_by')
+admin.site.register(models.Status, StatusAdmin)
--- my_python_blog/main/models.py
+++ my_python_blog/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.Model):
+	name		=	models.CharField(max_length = 32)
+	description	=	models.TextField(blank = True)
+	created_at	=	models.DateTimeField(auto_now_add=True)
+	updated_at	=	models.DateTimeField(auto_now = True)
+	created_by	=	models.CharField(max_length = 32)
+	updated_by	=	models.CharField(max_length = 32)
