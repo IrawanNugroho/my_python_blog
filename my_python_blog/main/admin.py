1149ca9767972428414cd945574c4054235d107f
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-01-14 13:33:37 +0700

    tag model with prepopulated field

--- my_python_blog/main/admin.py
+++ my_python_blog/main/admin.py
@@ -8,3 +8,9 @@ class StatusAdmin(admin.ModelAdmin):
     list_display    =   ('name', 'description', 'updated_by', 'updated_at')
     exclude         =   ('created_by', 'updated_by')
 admin.site.register(models.Status, StatusAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display        =   ('name', 'updated_by', 'updated_at')
+    exclude             =   ('created_by', 'updated_by')
+    prepopulated_fields =   {"slug":("name",)}
+admin.site.register(models.Tag, TagAdmin)
--- my_python_blog/main/models.py
+++ my_python_blog/main/models.py
@@ -3,8 +3,9 @@ from crum import get_current_user
 
 # Create your models here.
 class Status(models.Model):
-	name		=	models.CharField(max_length = 32)
-	description	=	models.TextField(blank = True)
+	name		=	models.CharField(max_length=32)
+	description	=	models.TextField(blank=True)
+	active		=	models.BooleanField(default=True)
 	created_at	=	models.DateTimeField(auto_now_add=True)
 	updated_at	=	models.DateTimeField(auto_now = True)
 	created_by	=	models.ForeignKey('auth.User', blank=True, null=True, default=None, on_delete=models.CASCADE, related_name='created_by')
@@ -18,3 +19,35 @@ class Status(models.Model):
 			self.created_by	=	user
 		self.updated_by	=	user
 		super(Status, self).save(*args, **kwargs)
+
+
+class Tag(models.Model):
+	name		=	models.CharField(max_length=32)
+	description	=	models.TextField(blank=True)
+	slug		=	models.SlugField(max_length=128)
+	created_at	=	models.DateTimeField(auto_now_add=True)
+	created_by	=	models.ForeignKey('auth.User', blank=True, null=True, default=None, on_delete=models.CASCADE, related_name='tag_created_by')
+	updated_at	=	models.DateTimeField(auto_now=True)
+	updated_by	=	models.ForeignKey('auth.User', blank=True, null=True, default=None, on_delete=models.CASCADE, related_name='tag_updated_by')
+
+	def save(self, *args, **kwargs):
+		user	=	get_current_user()
+		if user and not user.pk:
+			user	=	None
+		if not self.pk:
+			self.created_by	=	user
+		self.updated_by	=	user
+		super(Tag, self).save(*args, **kwargs)
+
+
+# class Image(models.Model):
+# 	title		=	
+# 	description	=
+# 	author		=
+# 	image		=
+# 	thumbnail	=
+# 	active		=
+# 	created_at	=
+# 	created_by	=
+# 	updated_at	=
+# 	updated_by	=
