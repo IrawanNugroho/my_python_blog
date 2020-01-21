85d419739a583a7b16b54fbdbbdc7cc36ffc3a6d
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-01-21 15:00:30 +0700

    single page done

--- my_python_blog/main/urls.py
+++ my_python_blog/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from main import models
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name ="home.html"), name="home"),
+    path("blog/<slug:slug>/", DetailView.as_view(model=models.Article), name="article"),
 ]
