76a6d134bbc67bcf7d798e5422328eb12d2fab28
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-03-24 13:47:32 +0700

    latest masih errror untuk menampilkan data latest articles di base html

--- my_python_blog/main/views.py
+++ my_python_blog/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article
+
+
+def index(request):
+    latest_article_list = Article.objects.order_by('updated_by')[:5]
+    context = {'latest_article_list': latest_article_list}
+    return render(request, 'main/templates/base.html', context)
