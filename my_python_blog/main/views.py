5a34d6de75fdb4a3032b395b27f9c08dda676030
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-03-27 17:11:21 +0700

    Update views.py

--- my_python_blog/main/views.py
+++ my_python_blog/main/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
-import json
-from django.core import serializers
-from django.http import HttpResponse
 from .models import Article
 
 
 
 def index(request):
-    # latest_article_list = Article.objects.all()
-    # tmpJson = serializers.serialize("json", latest_article_list)
-    # tmpObj = json.loads(tmpJson)
-    # return HttpResponse(json.dumps(tmpObj))
     latest_article_list = Article.objects.order_by('updated_by')[:6]
     context = {'latest_article_list': latest_article_list}
     return render(request, 'base.html', context)
