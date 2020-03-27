85c4551970e6c5d7c4266fbc48e8e4b2d65a03e9
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-03-27 17:01:30 +0700

    listing article on home well done

--- my_python_blog/main/urls.py
+++ my_python_blog/main/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from main import models
+from . import views
 
 urlpatterns = [
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-    path("", TemplateView.as_view(template_name ="home.html"), name="home"),
+    # path("", TemplateView.as_view(template_name ="home.html"), name="home"),
+    path('', views.index, name='home'),
     path("blog/<slug:slug>/", DetailView.as_view(model=models.Article), name="article"),
 ]
--- my_python_blog/main/views.py
+++ my_python_blog/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+import json
+from django.core import serializers
+from django.http import HttpResponse
 from .models import Article
 
 
+
 def index(request):
-    latest_article_list = Article.objects.order_by('updated_by')[:5]
+    # latest_article_list = Article.objects.all()
+    # tmpJson = serializers.serialize("json", latest_article_list)
+    # tmpObj = json.loads(tmpJson)
+    # return HttpResponse(json.dumps(tmpObj))
+    latest_article_list = Article.objects.order_by('updated_by')[:6]
     context = {'latest_article_list': latest_article_list}
-    return render(request, 'main/templates/base.html', context)
+    return render(request, 'base.html', context)
