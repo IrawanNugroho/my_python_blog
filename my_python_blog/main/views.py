8167603253b2e8aa54d6cbd670d071ed8b63e435
Irawan Setyo Adhi Nugroho <irawansetyoadhi.nugroho3@gmail.com>
2020-04-01 12:11:17 +0700

    update view descending

--- my_python_blog/main/views.py
+++ my_python_blog/main/views.py
@@ -4,6 +4,6 @@ from .models import Article
 
 
 def index(request):
-    latest_article_list = Article.objects.order_by('updated_by')[:6]
+    latest_article_list = Article.objects.order_by('-updated_by')[:6]
     context = {'latest_article_list': latest_article_list}
     return render(request, 'base.html', context)
